2e7faa21528d2880ff7ea7df133bf851c49a0303
abudsistem <125583817+abudsistem@users.noreply.github.com>
2023-07-27 15:46:46 -0500

    a
    a

---
+++ app/about/page.js
@@ -0,0 +1,48 @@
+export default function AboutMe() {
+    return(
+        <>
+            <h2 >
+                hi, I'm abudsystem
+            </h2>
+            
+            <p > 
+                Do you need to create something? or do you want a solution?
+                <br></br>
+                here you have some information about me so you don't hesitate  
+            </p>
+
+            <p>
+               FULL-STACK  -  WEB DEVELOPMENT
+            </p>
+
+            <p>
+                My focus revolves around understanding the needs of clients and end-users, and then translating those needs into effective technical solutions. I employ a best-practices approach to web development and stay up-to-date with the latest trends and technologies to ensure my solutions are scalable, secure, and high-performing.</p>
+            <div>
+                <h3>Skills</h3>
+                
+                <div>
+                    <div>
+                        <h4>frontend</h4>
+                        <ul>
+                            a
+                        </ul>
+                    </div>
+                
+                    <div>
+                        <h4>backend</h4>
+                        <ul>
+                            a
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <h3> my mentors are the following sites and people </h3>
+            <div>
+                <ul>
+                   a
+                </ul>
+            </div>
+        </>
+    )
+}
--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import AboutMe from './about/page'
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <AboutMe />
       <div className={styles.description}>
         <p>
           Get started by editing&nbsp;
