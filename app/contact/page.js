3448d1dcd95c0797e66f6a997c82c16d1947add0
abudsistem <125583817+abudsistem@users.noreply.github.com>
2023-08-04 00:10:14 -0500

    cambios de formato y el contact

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,8 +1,32 @@
-export default function AboutMe() {
+'use client';
+import React,{  useState } from 'react';
+
+export default function Contact() {
+    const [name, setName ] = useState('your name ;)')
+
+    const changeName= (e) => {
+        const value = e.target.value;
+        setName(value)
+    }
+
+    const saveClick = () => console.log("save :", name);
+
     return(
-        <h1>
-            hola
-        </h1>
-    
+        <>
+            <h2>hi, there</h2>
+            
+            <p><strong>work with me!</strong></p>
+
+            <div>
+            <label> name: </label>
+            <input
+            id='name'
+            name='name'
+            value={name}
+            onChange={changeName}
+            ></input>
+            <button onClick={saveClick}> save </button>
+            </div>
+        </>
     )
 }
