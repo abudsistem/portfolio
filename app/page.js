a16a64b87fc3202c8d64de8286665924d583b548
abudsistem <125583817+abudsistem@users.noreply.github.com>
2023-08-03 22:13:27 -0500

    prueba de textos
    prueba

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import AboutMe from './about/page'
+import Navbar from './components/layout/navbar/page'
 
 export default function Home() {
   return (
-    <main className={styles.background}>
+    <main className={`${styles.background}`}>
+      <Navbar className={`${styles.navbar} `}/>
 
       <AboutMe />
       
