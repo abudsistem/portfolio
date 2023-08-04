3d16faffe7569838d1bbe731f2f05226dc79830e
abudsistem <125583817+abudsistem@users.noreply.github.com>
2023-08-04 00:16:10 -0500

    about me cmabios

--- app/about/page.js
+++ app/about/page.js
@@ -5,12 +5,48 @@ import Mentors from "../components/skills/mentors/page";
 export default function AboutMe() {
     return(
         <>
-            <h1>hola</h1>
-            <Skillsfrontend />
-            <Skillsbackend />
-            <Mentors />
+            <h2>
+                hi, I'm abudsystem
+            </h2>
+            
+            <p> 
+                Do you need to create something? or do you want a solution?
+                <br></br>
+                here you have some information about me so you don't hesitate  
+            </p>
+
+            <p >
+               FULL-STACK  -  WEB DEVELOPMENT
+            </p>
+
+            <p>
+                My focus revolves around understanding the needs of clients and end-users, and then translating those needs into effective technical solutions. I employ a best-practices approach to web development and stay up-to-date with the latest trends and technologies to ensure my solutions are scalable, secure, and high-performing.</p>
+            <div>
+                <h3>Skills</h3>
+                
+                <div>
+                    <div>
+                        <h4>frontend</h4>
+                        <ul>
+                            <Skillsfrontend />
+                        </ul>
+                    </div>
+                
+                    <div>
+                        <h4>backend</h4>
+                        <ul>
+                            <Skillsbackend />
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <h3> my mentors are the following sites and people </h3>
+            <div>
+                <ul>
+                   <Mentors />
+                </ul>
+            </div>
         </>
-        
-    
     )
 }
