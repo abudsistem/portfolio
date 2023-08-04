ada64f9d9342d72fa37c54a9b19112ee8927a5b2
abudsistem <125583817+abudsistem@users.noreply.github.com>
2023-08-03 23:54:38 -0500

    fsd
    fasd

--- app/page.js
+++ app/page.js
@@ -1,10 +1,7 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import AboutMe from './about/page'
-import Navbar from './components/layout/navbar/navBar'
-
-
-
+import Navbar from './components/layout/navbar/page'
 
 export default function Home() {
   return (
