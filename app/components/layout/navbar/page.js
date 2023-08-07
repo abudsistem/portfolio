f2184e0c7aeba639da0c17b98ab43db9fd59629f
abudsistem <125583817+abudsistem@users.noreply.github.com>
2023-08-07 15:53:59 -0500

    viendo cambios

--- app/components/layout/navbar/page.js
+++ app/components/layout/navbar/page.js
@@ -3,7 +3,7 @@ import React from 'react';
 import styles from '../../../page.module.css'
 export default function Navbar() {
     return(
-            <nav className={`${styles.nav}`}>
+            <nav className={`${styles.navBar}`}>
                 <ul>
                     <li className={`${styles.navItem}`}>
                         <Link href='/hero'>
