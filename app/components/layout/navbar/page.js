dab4160959f7666d788404c477cdeb0de5899db6
abudsistem <125583817+abudsistem@users.noreply.github.com>
2023-08-07 16:58:49 -0500

    nav items

--- app/components/layout/navbar/page.js
+++ app/components/layout/navbar/page.js
@@ -4,7 +4,7 @@ import styles from '../../../page.module.css'
 export default function Navbar() {
     return(
             <nav className={`${styles.navBar}`}>
-                <ul>
+                <ul className={`${styles.navContainer}`}>
                     <li className={`${styles.navItem}`}>
                         <Link href='/hero'>
                             HERO
