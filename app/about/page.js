33b33a5e291179cb62ecd00b82121c3e43231bee
abudsistem <125583817+abudsistem@users.noreply.github.com>
2023-08-09 10:44:17 -0500

    ver si asi fuynciona

--- app/about/page.js
+++ app/about/page.js
@@ -5,10 +5,10 @@ import Mentors from "../components/skills/mentors/page";
 export default function AboutMe() {
     return(
         <header>
-            <h2>hi, I'm abudsystem</h2>
-                <p>Do you need to create something? or do you want a solution? .Here you have some information about me so you don't hesitate</p>
-                <p>FULL-STACK  -  WEB DEVELOPMENT</p>
-                <p>My focus revolves around understanding the needs of clients and end-users, and then translating those needs into effective technical solutions. I employ a best-practices approach to web development and stay up-to-date with the latest trends and technologies to ensure my solutions are scalable, secure, and high-performing.</p>
+            <h2>hi Im abudsystem</h2>
+                <p>Do you need to create something or do you want a solution Here you have some information about me so you dont hesitate</p>
+                <p>FULL-STACKWEB DEVELOPMENT</p>
+            <p>My focus revolves around understanding the needs of clients and endusers and then translating those needs into effective technical solutions I employ a bestpractices approach to web development and stay up-to-date with the latest trends and technologies to ensure my solutions are scalable secure and highperforming</p>
             <h3>Skills</h3>
                 <h4>frontend</h4>
                     <Skillsfrontend />
